fc0217ddcee32def1a06f3c705e08e42f4070950
vishnugprasad <vishnuanugraha001@gmail.com>
2022-03-31 09:38:14 +0530

    reverse a string

--- src/index.js
+++ src/index.js
@@ -148,61 +148,76 @@
 
 //.........................IMPLEMENTING AN ARRAY........................................
 
-class MyArray{
-    constructor() {
-        this.length = 0;
-        this.data = {};
-    }
+// class MyArray{
+//     constructor() {
+//         this.length = 0;
+//         this.data = {};
+//     }
 
-    get(index){
-        return this.data[index]
-    }
+//     get(index){
+//         return this.data[index]
+//     }
 
-    push(item){
-        this.data[this.length] = item;
-        this.length++
-        return this.length;
-    }
+//     push(item){
+//         this.data[this.length] = item;
+//         this.length++
+//         return this.length;
+//     }
 
-    pop(){
-        const lastItem = this.data[this.length-1]
-        delete this.data[this.length-1]
-        this.length--
-        return lastItem
-    }
+//     pop(){
+//         const lastItem = this.data[this.length-1]
+//         delete this.data[this.length-1]
+//         this.length--
+//         return lastItem
+//     }
 
-    delete(index){
-        const item = this.data[index]
+//     delete(index){
+//         const item = this.data[index]
 
-        this.shiftItem(index)
+//         this.shiftItem(index)
         
+//     }
+
+//     shiftItem(index){
+//         for( let i = index; i< this.length-1; i++){
+//             this.data[index] = this.data[index+1]
+//         }
+//         delete this.data[this.length-1];
+//         this.length--;
+//     }
+// }
+
+
+// const newArray = new MyArray();
+// newArray.push("vishnu");
+// newArray.push("G")
+// newArray.push("Prasad")
+// newArray.push("Anna")
+// newArray.push("Delhvi")
+
+
+// // console.log(newArray.get(0));
+// // newArray.pop();
+// console.log(newArray)
+// newArray.delete(3)
+// console.log(newArray);
+
+
+//........................REVERSE A STRING...........................
+
+function reverseString(str){
+    if(!str || str.length < 2|| typeof str !== "string"){
+        return "invalid operation"
     }
 
-    shiftItem(index){
-        for( let i = index; i< this.length-1; i++){
-            this.data[index] = this.data[index+1]
-        }
-        delete this.data[this.length-1];
-        this.length--;
+    const backwards = [];
+    const totalItems = str.length-1;
+    for( let i = totalItems; i >= 0; i--){
+        backwards.push(str[i]);
     }
+    console.log(backwards);
+    return backwards.join("")
 }
 
-
-const newArray = new MyArray();
-newArray.push("vishnu");
-newArray.push("G")
-newArray.push("Prasad")
-newArray.push("Anna")
-newArray.push("Delhvi")
-
-
-// console.log(newArray.get(0));
-// newArray.pop();
-console.log(newArray)
-newArray.delete(3)
-console.log(newArray);
-
-
-
-
+reverseString("my name is vishnu")
 
