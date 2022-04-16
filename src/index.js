1dd4eea374d6ed35d96d313aa0bfd6c920866a3a
vishnugprasad <vishnuanugraha001@gmail.com>
2022-04-16 09:17:36 +0530

    leetcode3

--- src/index.js
+++ src/index.js
@@ -401,39 +401,65 @@
 //...................LEETCODE 2......................
 
 
-var addTwoNumbers = function (l1,l2){
-  var ListNode;
-  let head = new ListNode(0);
-  let node = head
-  let carry = 0
+// var addTwoNumbers = function (l1,l2){
+//   // var ListNode;
+//   let head = new ListNode(0);
+//   let node = head
+//   let carry = 0
 
-  while(l1||l2){
+//   while(l1||l2){
 
-    let l1value = l1 ? l1.val : 0;
-    let l2value = l2 ? l2.val : 0;
+//     let l1value = l1 ? l1.val : 0;
+//     let l2value = l2 ? l2.val : 0;
     
-    let sum  = l1value + l2value + carry;
-    carry = 0
-    let newValue  = sum;
-    if(sum > 9){
-      newValue = sum % 10
-      carry = 1
-    }
-    node.next = new ListNode(newValue)
-    node =  node.next
+//     let sum  = l1value + l2value + carry;
+//     carry = 0
+//     let newValue  = sum;
+//     if(sum > 9){
+//       newValue = sum % 10
+//       carry = 1
+//     }
+//     node.next = new ListNode(newValue)
+//     node =  node.next
 
-    if(l1){
-      l1 = l1.next
+//     if(l1){
+//       l1 = l1.next
+//     }
+//     if(l2){
+//       console.log(l2);
+//       l2 = l2.next
+//     }
+//   }
+//   if (carry){
+//     node.next = new ListNode(carry)
+//   }
+//   return head.next
+// }
+
+// addTwoNumbers([2,4,3],[5,6,4])
+
+//...........................3. Longest Substring Without Repeating Character (LEETCODE)...................
+
+function lengthOfLongestSubstring(check) {
+  var letters = check.split("");
+  var max = 0;
+  var result = new Map();
+  var start = 0;
+  
+  for (var i = 0; i < letters.length; i++) {
+    if (!result.has(letters[i])) {
+      result.set(letters[i], i);
+    } else {
+      i = result.get(letters[i]);
+      result.clear();
     }
-    if(l2){
-      console.log(l2);
-      l2 = l2.next
+    
+    if (max < result.size) {
+      max = result.size;
     }
   }
-  if (carry){
-    node.next = new ListNode(carry)
-  }
-  return head.next
+  return max;
 }
 
-addTwoNumbers([2,4,3],[5,6,4])
+// Example:
+console.log(lengthOfLongestSubstring("dvdf")); // 3
