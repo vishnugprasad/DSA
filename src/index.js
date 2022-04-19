be80a38386bdf123151c893556d68030f62b18f0
vishnugprasad <vishnuanugraha001@gmail.com>
2022-04-19 19:43:47 +0530

    reverse integer

--- src/index.js
+++ src/index.js
@@ -466,29 +466,50 @@
 
 //.............................5. Longest Palindromic Substring................................
 
-const getDrome =  (left,right,s) => {
+// const getDrome =  (left,right,s) => {
 
-  while(left >= 0 && right < s.length){
-    if(s[left]!== s[right])break;
-    left--;
-    right++;
+//   while(left >= 0 && right < s.length){
+//     if(s[left]!== s[right])break;
+//     left--;
+//     right++;
 
+//   }
+
+//   return [ left+1, right]
+// }
+
+
+// var longestPalindrom = function(s){
+//   let max  = [0,1];
+
+//   for(let i =0; i<s.length; i++){
+//     let even = getDrome(i-1, i , s);
+//     let odd =  getDrome(i-1, i+1, s);
+//     let curMax = odd[1] - odd[0] > even[1] - even[0] ? odd : even;
+
+//     max = max[1] - max[0] > curMax[1] - curMax[0] ? max : curMax;
+
+//   }
+//   return s.slice(max[0], max[1])
+// }
+
+
+
+
+//.............................................
+
+
+var reverse = function (num) {
+  
+  const reversedNumber = parseInt(
+      Math.abs(num).toString().split('').reverse().join('')
+  );
+
+  
+  if (reversedNumber > 2147483647) {
+      return 0;
   }
 
-  return [ left+1, right]
-}
-
-
-var longestPalindrom = function(s){
-  let max  = [0,1];
-
-  for(let i =0; i<s.length; i++){
-    let even = getDrome(i-1, i , s);
-    let odd =  getDrome(i-1, i+1, s);
-    let curMax = odd[1] - odd[0] > even[1] - even[0] ? odd : even;
-
-    max = max[1] - max[0] > curMax[1] - curMax[0] ? max : curMax;
-
-  }
-  return s.slice(max[0], max[1])
-}
+  
+  return num < 0 ? -Math.abs(reversedNumber) : reversedNumber;
+};
