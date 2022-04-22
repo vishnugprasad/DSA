1423a3ad87154453f2406cfbf9dfa695d73bf2ab
vishnugprasad <vishnuanugraha001@gmail.com>
2022-04-22 15:01:10 +0530

    leetcode

--- src/index.js
+++ src/index.js
@@ -588,7 +588,23 @@
 //         }
 //     }
 // }
-
+ //..............leetcode.......................
+//  var convert = function(s, numRows) {
+//     if (s.length <= numRows || numRows < 2) return s;
+//     var len = s.length;
+//     var num = 2 * (numRows - 1);
+//     var res = Array(numRows).fill('');
+//     var tmp = 0;
+//     for (var i = 0; i < len; i++) {
+//       tmp = i % num;
+//       if (tmp < numRows) {
+//         res[tmp] += s[i];
+//       } else {
+//         res[num - tmp] += s[i];
+//       }
+//     }
+//     return res.join('');
+//   };
 
 class LinkedList {
     constructor(value){
