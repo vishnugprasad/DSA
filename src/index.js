2e7b11675262b0a7704ff038713845b51473eda0
vishnugprasad <vishnuanugraha001@gmail.com>
2022-04-22 06:50:34 +0530

    pointer

--- src/index.js
+++ src/index.js
@@ -536,29 +536,38 @@
 //......................13. Roman to Integer...................
 
 
-var romanToInt = function (s) {
+// var romanToInt = function (s) {
  
-  const romanMap = new Map();
+//   const romanMap = new Map();
  
-  romanMap.set('I', 1);
-  romanMap.set('V', 5);
-  romanMap.set('X', 10);
-  romanMap.set('L', 50);
-  romanMap.set('C', 100);
-  romanMap.set('D', 500);
-  romanMap.set('M', 1000);
+//   romanMap.set('I', 1);
+//   romanMap.set('V', 5);
+//   romanMap.set('X', 10);
+//   romanMap.set('L', 50);
+//   romanMap.set('C', 100);
+//   romanMap.set('D', 500);
+//   romanMap.set('M', 1000);
  
-  const n = s.length;
+//   const n = s.length;
   
-  let num = romanMap.get(s[n - 1]);
+//   let num = romanMap.get(s[n - 1]);
 
-  for (let i = n - 2; i >= 0; i--) {
+//   for (let i = n - 2; i >= 0; i--) {
       
-      if (romanMap.get(s[i]) >= romanMap.get(s[i + 1])) {
-          num += romanMap.get(s[i]);
-      } else {
-          num -= romanMap.get(s[i]);
-      }
-  }
-  return num;
-};
+//       if (romanMap.get(s[i]) >= romanMap.get(s[i + 1])) {
+//           num += romanMap.get(s[i]);
+//       } else {
+//           num -= romanMap.get(s[i]);
+//       }
+//   }
+//   return num;
+// };
+
+
+
+const obj1  = { a :true};
+const obj2 = obj1;
+obj1.a = "vishnu"
+// delete obj1
+console.log(obj1); // pointer....pinting to the same memory location
+console.log(obj2);
