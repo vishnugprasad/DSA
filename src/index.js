7ddb00af91b2dd9da138cf516e2fb538c4ba4f87
vishnugprasad <vishnuanugraha001@gmail.com>
2022-04-24 08:11:43 +0530

    Container With Most Water

--- src/index.js
+++ src/index.js
@@ -628,7 +628,17 @@ var longestCommonPrefix = function (strs) {
     return strs[0];
 };
 
+//.............................Container With Most Water...............
 
+
+var maxArea = function(H) {
+    let ans = 0, i = 0, j = H.length-1
+    while (i < j) {
+        ans = Math.max(ans, Math.min(H[i], H[j]) * (j - i))
+        H[i] <= H[j] ? i++ : j--
+    }
+    return ans
+};
 
 
 class LinkedList {
